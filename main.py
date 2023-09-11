aba4a0fb539ace4bb57b6ba7cd4bb0326baa0f05
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-09-11 10:21:06 -0300

    feature(collectables): Caio's version of a player collecting objects

--- main.py
+++ main.py
@@ -1,62 +1,175 @@
 import pygame
-import sys
+from sys import exit
+import random
+
 
 pygame.init()
 
+teste = pygame.image.load('bob.jpg')
+
+tela = pygame.display.set_mode((800, 400))
+tela.fill('Red')
+
+pygame.display.set_caption('Prototipo')
+pygame.display.set_icon(teste)
+
+relogio = pygame.time.Clock()
+
+var = 0
+var2 = 0
+var3 = 0
+
+fonte = pygame.font.Font('Minecraft.ttf', 20)
+texto = fonte.render(f'Coletou {var} brancos, {var2} cinzas e {var3} pretos', False, 'Green')
+
 class Player:
-    # Make player and position
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, altura, largura):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.display_scroll = [0,0]
-        self.current_x_position = x
-        self.current_y_position = y
+        self.altura = altura
+        self.largura = largura
     
-    # Draw player
-    def main(self, display):
-        self.current_x_position = self.x+self.display_scroll[0]
-        self.current_y_position = self.y+self.display_scroll[1]
-        pygame.draw.rect(display, (255, 0, 0), (self.current_x_position, self.current_y_position, self.width, self.height))
+    def posicionar(self, tela):
+        pygame.draw.rect(tela, ('Blue'), (self.x, self.y, self.largura, self.altura))
+    
+    def get_posicao(self):
+        return (self.x, self.y)
 
-player = Player(400, 300, 32, 32)
+class Coletavel:
+    def __init__(self, x, y, largura, altura):
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+    
+    def posicionar_c(self, tela):
+        pygame.draw.rect(tela, ('White'), (self.x, self.y, self.largura, self.altura))
 
-screen_size = (800, 600)
 
-display = pygame.display.set_mode(screen_size)
+    def get_pc(self):
+        return(self.x, self.y)
+    
+class Coletavel2:
+    def __init__(self, x, y, largura, altura):
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+    
+    def posicionar_c(self, tela):
+        pygame.draw.rect(tela, ('Gray'), (self.x, self.y, self.largura, self.altura))
 
-clock = pygame.time.Clock()
 
+    def get_pc(self):
+        return(self.x, self.y)
+    
+class Coletavel3:
+    def __init__(self, x, y, largura, altura):
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+    
+    def posicionar_c(self, tela):
+        pygame.draw.rect(tela, ('Black'), (self.x, self.y, self.largura, self.altura))
+
+
+    def get_pc(self):
+        return(self.x, self.y)
+
+
+x_c = random.randint(0, 800)
+y_c = random.randint(0, 400)
+
+coletar = Coletavel(x_c, y_c, 15, 15)
+
+x_c2 = random.randint(0, 800)
+y_c2 = random.randint(0, 400)
+
+coletar2 = Coletavel2(x_c2, y_c2, 15, 15)
+
+x_c3 = random.randint(0, 800)
+y_c3 = random.randint(0, 400)
+
+coletar3 = Coletavel3(x_c3, y_c3, 15, 15)
+
+x = 400
+y = 200
+altura = 20
+largura = 20
+jogador = Player(x, y, altura, largura)
 
 while True:
-     # Display color
-    display.fill((24, 164, 86))
-    
-     # Game EXIT
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            pygame.QUIT
-    
-    # Player
-    player.main(display)
-    
-    # Move and Key press
-    keys = pygame. key.get_pressed ()
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        if player.current_x_position > 0:
-            player.display_scroll[0] -= 5
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        if player.current_x_position < screen_size[0] - player.width:
-            player.display_scroll[0] += 5
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        if player.current_y_position > 0:
-            player.display_scroll[1] -= 5
-    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        if player.current_y_position < screen_size[1] - player.height:
-            player.display_scroll[1] += 5
-    
-     # Display configurations
-    clock.tick(60)
+    for evento in pygame.event.get():
+        if(evento.type == pygame.QUIT):
+            pygame.quit()
+            exit()
+
+    texto = fonte.render(f'Coletou {var} brancos, {var2} cinzas e {var3} pretos', False, 'Green')
+
+    tela.blit(texto, (200, 200))
+
+    surf = pygame.Surface((jogador.largura, jogador.altura))
+    rect = surf.get_rect(center = (jogador.x, jogador.y))
+
+    surf2 = pygame.Surface((coletar.largura, coletar.altura))
+    rect2 = surf.get_rect(center = (coletar.x, coletar.y))
+
+    surf3 = pygame.Surface((coletar2.largura, coletar2.altura))
+    rect3 = surf.get_rect(center = (coletar2.x, coletar2.y))
+
+    surf4 = pygame.Surface((coletar3.largura, coletar3.altura))
+    rect4 = surf.get_rect(center = (coletar3.x, coletar3.y))
+
+    tela.blit(surf, rect)
+    tela.blit(surf2, rect2)
+    tela.blit(surf3, rect3)
+    tela.blit(surf4, rect4)
+
+    if rect.colliderect(rect2):
+        var += 1
+        x_c = random.randint(0, 800)
+        y_c = random.randint(0, 400)
+
+        coletar = Coletavel(x_c, y_c, 15, 15)
+        coletar.posicionar_c(tela)
+    elif rect.colliderect(rect3):
+        var2 += 1
+        x_c2 = random.randint(0, 800)
+        y_c2 = random.randint(0, 400)
+
+        coletar2 = Coletavel2(x_c2, y_c2, 15, 15)
+        coletar2.posicionar_c(tela)
+    elif rect.colliderect(rect4):
+        var3 += 1
+        x_c3 = random.randint(0, 800)
+        y_c3 = random.randint(0, 400)
+
+        coletar3 = Coletavel3(x_c3, y_c3, 15, 15)
+        coletar3.posicionar_c(tela)
+
+
+    keys = pygame.key.get_pressed()
+
+    if(keys[pygame.K_d]):
+        x += 3
+    elif(keys[pygame.K_s]):
+        y += 3
+    elif(keys[pygame.K_w]):
+        y -= 3
+    elif(keys[pygame.K_a]):
+        x -= 3
+
+
+    tela.fill('Red')
+    tela.blit(texto, (200, 200))
+
+    jogador = Player(x, y, altura, largura)
+    jogador.posicionar(tela)
+
+    coletar.posicionar_c(tela)
+    coletar2.posicionar_c(tela)
+    coletar3.posicionar_c(tela)
+
     pygame.display.update()
+    relogio.tick(60)
