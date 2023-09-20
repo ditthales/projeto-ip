22c921991ef06dc678205021f0f53090d28b07a7
rencab <144038696+rencab@users.noreply.github.com>
2023-09-20 12:56:52 -0300

    Update inimigo to PEP8

--- inimigo.py
+++ inimigo.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Inimigo:
     def __init__(self, x, y, largura, altura, cor):
         self.x = x
@@ -7,22 +8,22 @@ class Inimigo:
         self.largura = largura
         self.altura = altura
         self.cor = cor
-    
+
     def posicionar_in(self, tela):
         pygame.draw.rect(tela, self.cor, (self.x, self.y, self.largura, self.altura))
 
     def comportamento(self, tupla_jogador):
-        #tupla_jogador no formato (x, y)
-        if(tupla_jogador[0] > self.x):
+        # tupla_jogador no formato (x, y)
+        if tupla_jogador[0] > self.x:
             self.x += 1.5
-        if(tupla_jogador[1] > self.y):
+        if tupla_jogador[1] > self.y:
             self.y += 1.5
-        if(tupla_jogador[0] < self.x):
+        if tupla_jogador[0] < self.x:
             self.x -= 1.5
-        if(tupla_jogador[1] < self.y):
+        if tupla_jogador[1] < self.y:
             self.y -= 1.5
 
     def rect_inimigo(self):
         surface_inimigo = pygame.Surface((self.largura, self.altura))
-        rectangle_inimigo = surface_inimigo.get_rect(center = (self.x, self.y))
+        rectangle_inimigo = surface_inimigo.get_rect(center=(self.x, self.y))
         return rectangle_inimigo
