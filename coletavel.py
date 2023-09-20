5d66d9aa2dc68032d2303c145185cae95d377a35
rencab <144038696+rencab@users.noreply.github.com>
2023-09-20 12:57:11 -0300

    Update coletavel.py

--- coletavel.py
+++ coletavel.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Coletavel:
     def __init__(self, x, y, largura, altura, color):
         self.x = x
@@ -7,14 +8,14 @@ class Coletavel:
         self.largura = largura
         self.altura = altura
         self.color = color
-    
+
     def posicionar_c(self, tela):
         pygame.draw.rect(tela, self.color, (self.x, self.y, self.largura, self.altura))
 
     def rect_coleta(self):
         surface_coletavel = pygame.Surface((self.largura, self.altura))
-        rectangle_coletavel = surface_coletavel.get_rect(center = (self.x, self.y))
+        rectangle_coletavel = surface_coletavel.get_rect(center=(self.x, self.y))
         return rectangle_coletavel
 
     def get_pc(self):
-        return(self.x, self.y)
+        return self.x, self.y
