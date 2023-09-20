106b81d5b32d023bae4077ac761fb0066fef4808
rencab <144038696+rencab@users.noreply.github.com>
2023-09-20 12:57:39 -0300

    Update bloco.py

--- bloco.py
+++ bloco.py
@@ -1,11 +1,17 @@
 import pygame
 
+
 class Bloco(pygame.sprite.Sprite):
     def __init__(self, pos, group, argumento) -> None:
+
         super().__init__(group)
         self.argumento = argumento
-        if(argumento == 'pedra'):
+
+        if argumento == 'pedra':
+
             self.image = pygame.image.load('pedra2.png')
-        elif(argumento == 'areia'):
+
+        elif argumento == 'areia':
+
             self.image = pygame.image.load('areia.png')
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center=pos)
