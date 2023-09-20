e3c37c18ae7ca591936e81e5b1823de0c8470893
rencab <144038696+rencab@users.noreply.github.com>
2023-09-20 12:58:06 -0300

    Update barras.py

--- barras.py
+++ barras.py
@@ -1,40 +1,43 @@
 import pygame
 
+
 class Vida:
 
     def __init__(self):
         self.hp = 10
         self.tela = pygame.display.get_surface()
-    
+
     def desenhar_vida(self):
+
         pygame.draw.rect(self.tela, 'Red', (10, 330, (10 * self.hp), 20))
-    
+
     def curar(self):
-        if(self.hp < 10):
+        if self.hp < 10:
             self.hp += 1
-    
+
     def dano(self):
-        if(self.hp > 0):
+        if self.hp > 0:
             self.hp -= 1
-    
+
     def reviver(self):
         self.hp = 10
+
 
 class Sede:
 
     def __init__(self):
         self.sede = 6000
         self.tela = pygame.display.get_surface()
-    
+
     def desenhar_sede(self):
-        pygame.draw.rect(self.tela, 'aquamarine', (10, 350, (self.sede/50), 20))       
+        pygame.draw.rect(self.tela, 'aquamarine', (10, 350, (self.sede / 50), 20))
 
     def sede_passiva(self):
-        if(self.sede > 2):
+        if self.sede > 2:
             self.sede -= 3
 
     def sede_ativa(self):
-        if(self.sede > 59):
+        if self.sede > 59:
             self.sede -= 60
 
     def refrescar(self):
@@ -42,4 +45,3 @@ class Sede:
 
     def ressucitar(self):
         self.sede = 6000
-    
